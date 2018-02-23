735030ac3da7cee6566c5cc8deae45932aa4164c
Student <student@ib.polsl.pl>
2018-02-23 14:20:27 +0100

    Trzeci Commit -  USE_TRIGONOMETRY_DEGREE = true

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,44 @@
+#define _USE_MATH_DEFINES
+#include "LaborkaConfig.h"
 #include <iostream>
-#include <trygonometria.h>
+#include <math.h>
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include <trygonometria.h>
+#else
+	#include<cmath>
+#endif
+
 int main() 
 {
-	double s=degreemath::sin(45);
+	double degree=45;
+	double s;
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		s = degreemath::sin(degree);
+	#else
+		s = sin(M_PI/4.0);
+	#endif
 	std::cout << "sin(45) = "<< s << std::endl;
 
-	s=degreemath::cos(45);
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		s=degreemath::cos(degree);
+	#else
+		s = cos(M_PI/4.0);
+	#endif
 	std::cout << "cos(45) = "<< s << std::endl;
 	
-	s=degreemath::tan(45);
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		s=degreemath::tan(degree);
+	#else
+		s = tan(M_PI/4.0);
+	#endif
 	std::cout << "tan(45) = "<< s << std::endl;
 	
-	s=degreemath::ctan(45);
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		s=degreemath::ctan(degree);
+	#else
+		s = 1/tan(M_PI/4.0);
+	#endif
 	std::cout << "ctg(45) = "<< s << std::endl;
 
 	system("pause");
